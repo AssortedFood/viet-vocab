f3aebd603af6ab37ff556ee3899a53cb297bb698
oxi <hamoon.b.94@gmail.com>
2025-02-19 22:47:25 +0000

    feat: create schema.sql and the corresponding vocab.db sqlite3 database

---
+++ data/schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS vocab (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    word TEXT NOT NULL,
+    translation TEXT NOT NULL,
+    description TEXT,
+    category TEXT DEFAULT 'General',
+    familiarity INTEGER DEFAULT 0,  -- 0 = Not rated, 1 = 😟, 2 = 😐, 3 = 🙂
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
